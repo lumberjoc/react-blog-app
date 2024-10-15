447d72691cefee4a9dff488e8671520baf0a8fdd
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-10-14 21:52:48 -0700

    update: header and layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
-import '../styles/globals.scss';
+import React, { useEffect, useState } from 'react';
+import { Layout } from "../components";
 
+import '../styles/globals.scss';
 import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  ) 
 }
 
 export default MyApp
