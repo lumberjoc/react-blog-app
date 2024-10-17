bd12df56bfe58cdfb00130515d4c54ae50767c4b
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-10-17 08:49:22 -0700

    fix: remove

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,30 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import moment from 'moment';
-import Link from 'next/link';
-import { getRecentPosts } from "@/services";
+import React from 'react'
 
-
-const PostWidget = ({ categories, slug }) => {
-    const [relatedPosts, setRelatedPosts] = useState([]);
-
-    useEffect(() => {
-        if(slug) {
-            // Get similar posts by knowing the category and passing
-            // in the slug.
-            getSimilarPosts(category, slug)
-                .then((result) => setRelatedPosts(result))
-        } else {
-            // Otherwise, we aren't looking at a specific article 
-            // so return recent posts.
-            getRecentPosts()
-                .then((result) => setRelatedPosts(result))
-        }
-    }, [input])
-    return (
-        <div>
-            
-        </div>
-    )
+const PostWidget = () => {
+  return (
+    <div>PostWidget</div>
+  )
 }
 
 export default PostWidget
