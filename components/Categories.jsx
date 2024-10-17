6a8231cbfe39fca330f26b1d9c737ee37b3349c1
lumberjoc <132397996+lumberjoc@users.noreply.github.com>
2024-10-17 11:28:05 -0700

    update: title/box formatting

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Link from "next/link";
+
+import { getCategories } from "@/services";
 
 const Categories = () => {
-  return (
-    <div>Categories</div>
-  )
+    const [Categories, setCategories] = useState([]);
+    // fetch data using graphql and hygraph
+    useEffect(() => {
+      getCategories()
+        .then((newCategories) => setCategories(newCategories))
+    }, []); // Leave dependency array empty so we only call it at the start to fill in categories
+    
+    return (
+        <div className="bg-white shadow-lg rounded-lg p-8 mb-8 pb-12">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4"> 
+                {/* Check if slug exists 
+                    Yes: See Related Posts
+                    No: See Recent Posts
+                */}
+                Categories
+            </h3>
+        </div>
+    )
 }
 
 export default Categories
